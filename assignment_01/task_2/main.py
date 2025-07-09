2c0d62b309d2316a410db63d0e063040bee8d866
MahathiR27 <kingmahathir27@gmail.com>
2025-07-09 10:36:18 +0600

    used Dijkstra to find actual cost for all nodes

--- assignment_01/task_2/main.py
+++ assignment_01/task_2/main.py
@@ -1,3 +1,6 @@
+import math
+from heapq import *
+
 in_f = open("input.txt", 'r')
 
 v,e = map(int, in_f.readline().split())
@@ -6,7 +9,7 @@ v+=1
 graph = {i:[] for i in range(1,v)}
 in_f.readline()
 
-s,g = map(int,in_f.readline().split())
+start,goal = map(int,in_f.readline().split())
 in_f.readline()
 
 h = {}
@@ -19,6 +22,28 @@ for i in range(e):
   graph[a].append(b)
   graph[b].append(a)
 
-print(f'''Start: {s}; Goal: {g}
-Heuristic: {h}
-Graph: {graph}''')
+# print(f'''Start: {s}; Goal: {g}
+# Heuristic: {h}
+# Graph: {graph}''')
+
+actual_cost = {i:math.inf for i in range(1,v)}
+queue = []
+
+actual_cost[goal] = 0
+heappush(queue,(0, goal))
+
+def dijkstra(queue):
+  if len(queue) == 0:
+    return actual_cost
+  else:
+    w, v = heappop(queue)
+    for neighbor in graph[v]:
+      path_cost = w + 1
+      if path_cost < actual_cost[neighbor]:
+        actual_cost[neighbor] = path_cost
+        heappush(queue,(path_cost, neighbor))
+    return dijkstra(queue)
+
+dijkstra(queue)
+
+print("Actual Cost for each node to reach goal:", actual_cost)
