e74533e001f37125e252c16346e9221fde2cd3a5
MahathiR27 <kingmahathir27@gmail.com>
2025-07-17 01:41:14 +0600

    finished fitness() added wire() to calculate wire distance and box() calculate the bounding bit

--- assignment_02/main.py
+++ assignment_02/main.py
@@ -1,9 +1,12 @@
 import random
+import math
 
 grid = (25,25)
 blocks = ("ALU", "Cache", "Control Unit", "Register File", "Decoder", "Floating Unit")
 dim = {"ALU":(5,5), "Cache":(7,4), "Control Unit":(4,4), "Register File":(6,6), "Decoder":(5,3), "Floating Unit":(5,5)}
 connections = ((3,0), (2,0), (0,1), (3,5), (1,4), (4,5)) # Kon index er component er sathe kon index er component connect korte hobe
+
+overlap_penalty, wiring_penalty = 1000, 2
 
 population = []
 parent_f = {}
@@ -43,16 +46,70 @@ def overlap(node1,block_num1,node2,block_num2):
   a_top = y1+y1_add
   b_top = y2+y2_add
 
-  overlap = not (a_right <= b_left or a_left >= b_right or a_bottom >= b_top or a_top <= b_bottom)
+  overlap = not (a_right <= b_left or a_left >= b_right or a_bottom >= b_top or a_top <= b_bottom) # copy
 
   return overlap
 
+def wire(cromo):
+  wire_distance = 0
+  for c in connections:
+    i, j = c
+    
+    block1, block2 = blocks[i], blocks[j]
+    
+    x1, y1 = cromo[i] # LB Coordinate
+    x2, y2 = cromo[j]
+    
+    x1_add, y1_add = dim[block1] #Hight, Width
+    x2_add, y2_add = dim[block2]
+    
+    center1 = (x1+x1_add/2, y1+y1_add/2)
+    center2 = (x2+x2_add/2, y2+y2_add/2)
+    
+    wire_distance += math.sqrt((center2[0]-center1[0])**2+(center2[1]-center1[1])**2) #sqrt((x2 - x1)**2 + (y2 - y1)**2)
+  
+  return wire_distance
+
+def box(cromo):
+  x_max, y_max = cromo[0]
+  x_min, y_min = cromo [0]
+  
+  for i in cromo:
+    if i[0] > x_max:
+      x_max = i[0]
+    elif i[0] < x_min:
+      x_min = i[0]
+    
+    if i[1] > y_max:
+      y_max = i[1]
+    elif i[1] < y_min:
+      y_min = i[1]
+  
+  return (x_max - x_min) * (y_max - y_min) # Copy
+
+
+def fitness(cromo):
+  # Count overlap count of a cromo
+  overlap_count = 0
+  for i in range (0,5):
+    for j in range (i+1,6):
+      if overlap(cromo[i],i,cromo[j],j):
+        overlap_count+=1
+        #print(f"Overlap: {blocks[i]} {cromo[i]}, {blocks[j]} {cromo[j]}")
+
+  wire_distance = round(wire(cromo),2)
+  bounding_box = box(cromo)
+  
+  fitness_value = -(overlap_penalty*overlap_count + wiring_penalty*wire_distance + bounding_box)
+  
+  print("Overlap:",overlap_count)
+  print("Wire Distance:",wire_distance)
+  print("Bounding Box:",bounding_box)
+  print("Fitness value:",fitness_value)
+  
 def ga(population):
   for cromo in population:
-    for i in range (0,5):
-      for j in range (i+1,6):
-        if overlap(cromo[i],i,cromo[j],j):
-          print(f"Overlap: {blocks[i]} {cromo[i]}, {blocks[j]} {cromo[j]}")
+    fitness(cromo)
 
 for j in range (6): # Creating start population
   x = []
