919ef0be01c00713490d0059ab47fccb9b32a97e
MahathiR27 <kingmahathir27@gmail.com>
2025-07-16 23:25:51 +0600

    added overlap checker tested with population sample

--- assignment_02/main.py
+++ assignment_02/main.py
@@ -11,12 +11,8 @@ child_f = {}
 
 def grid_checker(co_ordinate,block_num): # block_num = component er index number to get the dimension
   block = blocks[block_num]
-  
-  x = co_ordinate[0]
-  y = co_ordinate[1]
-  
-  x_add = dim[block][0] # Hight
-  y_add = dim[block][1] # Width
+  x, y = co_ordinate
+  x_add, y_add = dim[block] # Hight, Width
   
   if x+x_add>grid[0] or y+y_add>grid[1]: 
     return False
@@ -29,6 +25,35 @@ def generate_node(block_num): # Random ekta generate korbe and block_number wise
   #print("Regenerate")
   return generate_node(block_num)
 
+def overlap(node1,block_num1,node2,block_num2):
+  block1, block2 = blocks[block_num1], blocks[block_num2] # kona kon component
+  
+  x1, y1 = node1 # LB Coordinate
+  x2, y2 = node2
+  
+  x1_add, y1_add = dim[block1] #Hight, Width
+  x2_add, y2_add = dim[block2]
+  
+  a_left = x1
+  b_left = x2
+  a_right = x1+x1_add
+  b_right = x2+x2_add
+  a_bottom = y1
+  b_bottom = y2
+  a_top = y1+y1_add
+  b_top = y2+y2_add
+
+  overlap = not (a_right <= b_left or a_left >= b_right or a_bottom >= b_top or a_top <= b_bottom)
+
+  return overlap
+
+def ga(population):
+  for cromo in population:
+    for i in range (0,5):
+      for j in range (i+1,6):
+        if overlap(cromo[i],i,cromo[j],j):
+          print(f"Overlap: {blocks[i]} {cromo[i]}, {blocks[j]} {cromo[j]}")
+
 for j in range (6): # Creating start population
   x = []
   for i in range (6):
@@ -37,3 +62,12 @@ for j in range (6): # Creating start population
 
 for i in population:
   print(i)
+
+# population = [[(9, 6), (7, 12), (1, 0), (10, 9), (16, 6), (16, 5)],
+# [(18, 19), (15, 14), (19, 17), (10, 3), (11, 20), (3, 17)],
+# [(9, 17), (8, 4), (4, 20), (10, 10), (2, 5), (20, 15)],
+# [(19, 9), (17, 14), (0, 9), (16, 19), (4, 10), (16, 4)],
+# [(17, 18), (13, 2), (5, 19), (12, 19), (20, 0), (7, 7)],
+# [(18, 0), (9, 2), (6, 17), (6, 9), (0, 20), (0, 13)]]
+
+ga(population)
