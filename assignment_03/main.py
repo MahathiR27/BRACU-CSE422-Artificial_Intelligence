86ade9a510465f9191ac7310494dedd7744126df
MahathiR27 <kingmahathir27@gmail.com>
2025-07-24 02:49:54 +0600

    added utility_gen() for equal length sequences

--- assignment_03/main.py
+++ assignment_03/main.py
@@ -7,14 +7,25 @@ std_id = list(map(int, in_f.readline().strip().split()))[-len(target):]
 def minimax(nuces, sequence=''):
   if len(sequence) == len(all_nuces):
     # Utility
+    x = utility_gen(sequence)
+    print(x)
     print(sequence)
     sequence = ''
-    return
+    return x
   else:
     for i in nuces:
-      next_nuces = nuces.copy()
+      next_nuces = nuces.copy() # Copy create kore jeta already add kora hoye gese oita remove kore diye baki tar upor recursion
       next_nuces.remove(i)
   
-      minimax(next_nuces, sequence + i)
+      utility = minimax(next_nuces, sequence + i) # loop er element ta ke sequence e add kore recurssion e send kortese
+
+def utility_gen(temp_sequence):
+  sequence_len = len(temp_sequence)
+  sum = 0
+  
+  if sequence_len == len(target):
+    for i in range(sequence_len):
+      sum -= std_id[i]*abs(ord(temp_sequence[i])-ord(target[i]))
+  return sum
 
 minimax(all_nuces)
