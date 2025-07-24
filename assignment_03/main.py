7d44f5ffdff10620d605cfd493600904c211c231
MahathiR27 <kingmahathir27@gmail.com>
2025-07-24 11:20:30 +0600

    completed utility_gen()

--- assignment_03/main.py
+++ assignment_03/main.py
@@ -7,11 +7,7 @@ std_id = list(map(int, in_f.readline().strip().split()))[-len(target):]
 def minimax(nuces, sequence=''):
   if len(sequence) == len(all_nuces):
     # Utility
-    x = utility_gen(sequence)
-    print(x)
-    print(sequence)
-    sequence = ''
-    return x
+    return utility_gen(sequence)
   else:
     for i in nuces:
       next_nuces = nuces.copy() # Copy create kore jeta already add kora hoye gese oita remove kore diye baki tar upor recursion
@@ -20,12 +16,30 @@ def minimax(nuces, sequence=''):
       utility = minimax(next_nuces, sequence + i) # loop er element ta ke sequence e add kore recurssion e send kortese
 
 def utility_gen(temp_sequence):
-  sequence_len = len(temp_sequence)
+  sequence_len = len(temp_sequence) - len(target)
   sum = 0
+  print(temp_sequence)
+  print(target)
+  print(sequence_len)
   
-  if sequence_len == len(target):
-    for i in range(sequence_len):
+  if sequence_len == 0: # Sob gula thik thak moto thakle id*(new-target)
+    for i in range(len(temp_sequence)):
       sum -= std_id[i]*abs(ord(temp_sequence[i])-ord(target[i]))
+  
+  elif sequence_len > 0: # Sequence jodi boro hoy
+    for i in range (sequence_len):
+      sum -= std_id[i]*abs(ord(temp_sequence[i])-ord(target[i]))
+    
+    for i in temp_sequence[len(target):]: # Target er len er por theke baki sequence sum e nitese
+      sum -= ord(i)
+      
+  else:
+    for i in range (abs(sequence_len)):
+      sum -= std_id[i]*abs(ord(temp_sequence[i])-ord(target[i]))
+    
+    for i in target[len(temp_sequence):]: # Sequence er len er por theke baki target sum e nitese
+      sum -= ord(i)
+  
   return sum
 
 minimax(all_nuces)
